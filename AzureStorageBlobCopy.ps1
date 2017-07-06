ee967a9f2016280d7f35281d7166aee05cbf5f39
Don Ky <don.d.ky@gmail.com>
2017-07-06 12:17:39 -0400

    Initial commit

---
+++ AzureStorageBlobCopy.ps1
@@ -0,0 +1,49 @@
+﻿### Source VHD
+$srcVhd = "src.vhd"
+$srcUri = "https://.blob.core.windows.net/vhds/$srcVhd"
+
+### Source Storage Account (West US) ###
+$srcStorageAccount = ""
+$srcStorageKey = ""
+
+### Target Storage Account (West US) ###
+$destStorageAccount = ""
+$destStorageKey = ""
+
+### Target Container Name ###
+$containerName = "vhds"
+
+### Target VHD Name ###
+$targetVhd = ".vhd"
+ 
+### Create the source storage account context ###
+$srcContext = New-AzureStorageContext  –StorageAccountName $srcStorageAccount `
+                                        -StorageAccountKey $srcStorageKey
+ 
+### Create the destination storage account context ### 
+$destContext = New-AzureStorageContext  –StorageAccountName $destStorageAccount `
+                                        -StorageAccountKey $destStorageKey
+ 
+### Create the container on the destination ###
+New-AzureStorageContainer -Name $containerName -Context $destContext
+ 
+### Start the asynchronous copy - specify the source authentication with -SrcContext ###
+$blob1 = Start-AzureStorageBlobCopy -srcUri $srcUri `
+                                    -SrcContext $srcContext `
+                                    -DestContainer $containerName `
+                                    -DestBlob $targetVhd `
+                                    -DestContext $destContext
+
+### Retrieve the current status of the copy operation ###
+$status = $blob1 | Get-AzureStorageBlobCopyState 
+
+### Print out status ###
+$status 
+
+### Loop until complete ###
+While($status.Status -eq "Pending"){
+  $status = $blob1 | Get-AzureStorageBlobCopyState
+  Start-Sleep 10
+  ### Print out status ###
+  $status
+}
